9fa01a6a52d206f1c2f8ec6a00eb93e46e1c31ef
PruStephan <prustephan98@gmail.com>
2023-08-04 22:24:11 +0200

    Test build and print binaries

--- Formula/tezos-smart-rollup-client-PtMumbai.rb
+++ Formula/tezos-smart-rollup-client-PtMumbai.rb
@@ -19,7 +19,7 @@ class TezosSmartRollupClientPtmumbai < Formula
     depends_on dependency => :build
   end
 
-  dependencies = %w[gmp hidapi libev libffi tezos-sapling-params]
+  dependencies = %w[gmp hidapi libev tezos-sapling-params]
   dependencies.each do |dependency|
     depends_on dependency
   end
