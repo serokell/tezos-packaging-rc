dd0887e1ba2db35ce28ad04515fdc2d1167f0824
Savely Krendelhoff <alicecandyom@proton.me>
2023-08-17 20:36:46 +0300

    [Chore] Bump Tezos sources to v18.0-rc1

--- scripts/update-tezos.sh
+++ scripts/update-tezos.sh
@@ -8,14 +8,14 @@
 
 set -e
 
-git config user.name "serokell-bot" # necessary for pushing
-git config user.email "tezos-packaging@serokell.io"
+git config user.name "Savely Krendelhoff" # necessary for pushing
+git config user.email "alicecandyom@proton.me"
 git fetch --all
 
 # Get latest tag from tezos/tezos
 git clone https://gitlab.com/tezos/tezos.git upstream-repo
 cd upstream-repo
-latest_upstream_tag_hash="$(git rev-list --tags --max-count=1)"
+latest_upstream_tag_hash="f59e1b0eaa26e97a23824dc4fbd4af2bd8fcf8af"
 latest_upstream_tag="$(git describe --tags "$latest_upstream_tag_hash")"
 opam_repository_tag='' # will be set by version.sh
 git checkout "$latest_upstream_tag"
@@ -39,21 +39,21 @@ if [[ "$latest_upstream_tag" != "$our_tezos_tag" ]]; then
 
     ./scripts/update-input.py tezos "$latest_upstream_tag_hash"
     ./scripts/update-input.py opam-repository "$opam_repository_tag"
-    git commit -a -m "[Chore] Bump Tezos sources to $latest_upstream_tag" --gpg-sign="tezos-packaging@serokell.io"
+    git commit -a -m "[Chore] Bump Tezos sources to $latest_upstream_tag" --gpg-sign="alicecandyom@proton.me"
 
     ./scripts/update-brew-formulae.sh "$latest_upstream_tag-1"
-    git commit -a -m "[Chore] Update brew formulae for $latest_upstream_tag" --gpg-sign="tezos-packaging@serokell.io"
+    git commit -a -m "[Chore] Update brew formulae for $latest_upstream_tag" --gpg-sign="alicecandyom@proton.me"
 
     sed -i 's/"release": "[0-9]\+"/"release": "1"/' ./meta.json
     # Update version of tezos-baking package
     sed -i "s/version = .*/version = \"$latest_upstream_tag\"/" ./baking/pyproject.toml
     # Commit may fail when release number wasn't updated since the last release
-    git commit -a -m "[Chore] Reset release number for $latest_upstream_tag" --gpg-sign="tezos-packaging@serokell.io" || \
+    git commit -a -m "[Chore] Reset release number for $latest_upstream_tag" --gpg-sign="alicecandyom@proton.me" || \
       echo "release number wasn't updated"
 
     sed -i 's/letter_version *= *"[a-z]"/letter_version = ""/' ./docker/package/model.py
     # Commit may fail when the letter version wasn't updated since the last release
-    git commit -a -m "[Chore] Reset letter_version for $latest_upstream_tag" --gpg-sign="tezos-packaging@serokell.io" || \
+    git commit -a -m "[Chore] Reset letter_version for $latest_upstream_tag" --gpg-sign="alicecandyom@proton.me" || \
       echo "letter_version wasn't reset"
 
     git push --set-upstream origin "$branch_name"
